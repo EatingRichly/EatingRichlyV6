dbfd8a8ed94f8d4016b4f1f256b102fff463cbbd
Eric J <wormeyman@gmail.com>
2017-03-19 21:14:46 -0700

    Whitespace cleanup

--- footer.php
+++ footer.php
@@ -6,35 +6,35 @@
 */
 ?>
 
-<?php if(is_front_page() && !is_paged() ) { 
-    get_template_part('parts/frontpage', 'large'); 
-    get_template_part('parts/frontpage', 'vertical'); 
+<?php if(is_front_page() && !is_paged() ) {
+  get_template_part('parts/frontpage', 'large');
+  get_template_part('parts/frontpage', 'vertical');
 } ?>
-        <?php get_sidebar('footer'); ?>
+  <?php get_sidebar('footer'); ?>
 
-        <!-- Footer -->
-        <div class="footer">
+  <!-- Footer -->
+  <div class="footer">
 
-            <?php if ( is_active_sidebar( 'footer-row-3-center' ) ) { ?>
-            <div class="footer-row-3-center"><?php dynamic_sidebar( 'footer-row-3-center' ); ?>
-            <?php } ?>
+    <?php if ( is_active_sidebar( 'footer-row-3-center' ) ) { ?>
+    <div class="footer-row-3-center"><?php dynamic_sidebar( 'footer-row-3-center' ); ?>
+    <?php } ?>
 
-            <?php $kale_footer_copyright = kale_get_option('kale_footer_copyright'); ?>
-            <?php if($kale_footer_copyright) { ?>
-            <div class="footer-copyright"><?php echo wp_kses_post($kale_footer_copyright); ?></div>
-            <?php } ?>
-            <?php /*
-            <div class="footer-copyright">
-                <ul class="credit">
-                    <li><?php _e('Built using ', 'kale'); ?><a href="https://www.lyrathemes.com/kale-pro"><?php _e('Kale Pro', 'kale'); ?></a> <?php _e('by', 'kale'); ?> <a href="https://www.lyrathemes.com">LyraThemes</a>.</li>
-                </ul>
-            </div>
-             */ ?>
+    <?php $kale_footer_copyright = kale_get_option('kale_footer_copyright'); ?>
+    <?php if($kale_footer_copyright) { ?>
+    <div class="footer-copyright"><?php echo wp_kses_post($kale_footer_copyright); ?></div>
+    <?php } ?>
+<?php /*
+    <div class="footer-copyright">
+        <ul class="credit">
+            <li><?php _e('Built using ', 'kale'); ?><a href="https://www.lyrathemes.com/kale-pro"><?php _e('Kale Pro', 'kale'); ?></a> <?php _e('by', 'kale'); ?> <a href="https://www.lyrathemes.com">LyraThemes</a>.</li>
+        </ul>
+    </div>
+*/ ?>
 
-        </div>
-        <!-- /Footer -->
+  </div>
+  <!-- /Footer -->
 
-    </div><!-- /Container -->
+  </div><!-- /Container -->
 </div><!-- /Main Wrapper -->
 
 <?php wp_footer(); ?>
