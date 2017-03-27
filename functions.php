1b9634a742d9600c406cd4b6065dd5d25632066d
Eric J <wormeyman@gmail.com>
2017-03-27 12:13:19 -0700

    Increase priority to make 2:3 images.

--- functions.php
+++ functions.php
@@ -3,21 +3,21 @@
 // https://gist.github.com/lyrathemes/7f5c791b7351b7cadd7ab9faaba0b204
 add_action( 'wp_enqueue_scripts', 'kale_child_enqueue_styles' );
 function kale_child_enqueue_styles() {
-  
+
   $parent_style = 'kale-style';
   $deps = array('bootstrap', 'bootstrap-select', 'font-awesome', 'owl-carousel');
   wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' , $deps);
-  
+
   wp_enqueue_style( 'kale-style-child', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version') );
 }
 
 function kale_get_option($key){
   global $kale_defaults;
-  
+
   $parent_theme = get_template_directory();
   $parent_theme_slug = basename($parent_theme);
   $parent_theme_mods = get_option( "theme_mods_{$parent_theme_slug}");
-  
+
   $value = '';
   $child_value = get_theme_mod($key);
   if(!empty($child_value)){
@@ -36,4 +36,4 @@ function kale_child_setup() {
   set_post_thumbnail_size( 400, 600, true );
   add_image_size( 'kale-thumbnail', 400, 600, true );
 }
-add_action( 'after_setup_theme', 'kale_child_setup' );
+add_action( 'after_setup_theme', 'kale_child_setup', 15 );
