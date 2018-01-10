4ce66fb60b66c2c633a6d702a8140b724d051d70
Eric J <wormeyman@gmail.com>
2018-01-10 13:46:22 -0800

    Formatting and Cleanup

--- footer.php
+++ footer.php
@@ -7,15 +7,14 @@
 ?>
 
 <?php
-if(is_front_page() && !is_paged() ) {
+if (is_front_page() && !is_paged()) {
     $kale_frontpage_order = kale_get_option('kale_frontpage_order');
-    if ( ! empty( $kale_frontpage_order ) && is_array( $kale_frontpage_order ) ) {
-
+    if (! empty($kale_frontpage_order) && is_array($kale_frontpage_order)) {
         $key = array_search('kale_frontpage_order_feed', $kale_frontpage_order);
         $after_feed = array_slice($kale_frontpage_order, $key);
 
-        foreach($after_feed as $section){
-            switch($section){
+        foreach ($after_feed as $section) {
+            switch ($section) {
                 case 'kale_frontpage_order_banner':             get_template_part('parts/frontpage', 'banner');  break;
                 case 'kale_frontpage_order_featured_posts':     get_template_part('parts/frontpage', 'featured');  break;
                 case 'kale_frontpage_order_large':              get_template_part('parts/frontpage', 'large');  break;
@@ -30,14 +29,18 @@ if(is_front_page() && !is_paged() ) {
   <!-- Footer -->
   <div class="footer">
 
-    <?php if ( is_active_sidebar( 'footer-row-3-center' ) ) { ?>
-    <div class="footer-row-3-center"><?php dynamic_sidebar( 'footer-row-3-center' ); ?>
-    <?php } ?>
+    <?php if (is_active_sidebar('footer-row-3-center')) {
+    ?>
+    <div class="footer-row-3-center"><?php dynamic_sidebar('footer-row-3-center'); ?>
+    <?php
+} ?>
 
     <?php $kale_footer_copyright = kale_get_option('kale_footer_copyright'); ?>
-    <?php if($kale_footer_copyright) { ?>
+    <?php if ($kale_footer_copyright) {
+        ?>
     <div class="footer-copyright"><?php echo wp_kses_post($kale_footer_copyright); ?></div>
-    <?php } ?>
+    <?php
+    } ?>
 <?php /*
     <div class="footer-copyright">
         <ul class="credit">
