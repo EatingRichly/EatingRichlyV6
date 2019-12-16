ffb6c7a76b9fed819b1d3497db1b2d0d5551a9f9
Eric J <wormeyman@gmail.com>
2019-12-16 14:32:14 -0800

    Footer port changes from 2.6

--- footer.php
+++ footer.php
@@ -2,36 +2,38 @@
 /**
 * The template for displaying the footer
 * EATING RICHLY
-* 2019-06-18 14:39:04
+* 2019-12-16 14:32:01
 *
 * @package kale
 */
-?>
 
-<?php if (is_front_page() && !is_paged()) {
-  $kale_frontpage_order = kale_get_option('kale_frontpage_order');
-  if (!empty($kale_frontpage_order) && is_array($kale_frontpage_order)) {
-    if (in_array('kale_frontpage_order_feed', $kale_frontpage_order)) {
-      $key = array_search('kale_frontpage_order_feed', $kale_frontpage_order);
-      $after_feed = array_slice($kale_frontpage_order, $key);
-      foreach ($after_feed as $section) {
-        switch ($section) {
-          case 'kale_frontpage_order_banner':
-            get_template_part('parts/frontpage', 'banner');
-            break;
-          case 'kale_frontpage_order_featured_posts':
-            get_template_part('parts/frontpage', 'featured');
-            break;
-          case 'kale_frontpage_order_large':
-            get_template_part('parts/frontpage', 'large');
-            break;
-          case 'kale_frontpage_order_vertical':
-            get_template_part('parts/frontpage', 'vertical');
-            break;
+if (is_front_page() && !is_paged()) {
+    $kale_frontpage_order = kale_get_option('kale_frontpage_order');
+    if (!empty($kale_frontpage_order) && is_array($kale_frontpage_order)) {
+        if (in_array('kale_frontpage_order_feed', $kale_frontpage_order)) {
+            $key = array_search(
+                'kale_frontpage_order_feed',
+                $kale_frontpage_order
+            );
+            $after_feed = array_slice($kale_frontpage_order, $key);
+            foreach ($after_feed as $section) {
+                switch ($section) {
+                    case 'kale_frontpage_order_banner':
+                        get_template_part('parts/frontpage', 'banner');
+                        break;
+                    case 'kale_frontpage_order_featured_posts':
+                        get_template_part('parts/frontpage', 'featured');
+                        break;
+                    case 'kale_frontpage_order_large':
+                        get_template_part('parts/frontpage', 'large');
+                        break;
+                    case 'kale_frontpage_order_vertical':
+                        get_template_part('parts/frontpage', 'vertical');
+                        break;
+                }
+            }
         }
-      }
     }
-  }
 } ?>
   <?php get_sidebar('footer'); ?>
 
@@ -40,23 +42,36 @@
 
     <?php if (is_active_sidebar('footer-row-3-center')) { ?>
     <div class="footer-row-3-center"><?php dynamic_sidebar(
-      'footer-row-3-center'
+        'footer-row-3-center'
     ); ?>
     <?php } ?>
 
-    <?php $kale_footer_copyright = kale_get_option('kale_footer_copyright'); ?>
+    <?php $kale_footer_copyright = kale_get_option(
+      'kale_footer_copyright'
+      ); ?>
     <?php if ($kale_footer_copyright) { ?>
     <div class="footer-copyright"><?php echo wp_kses_post(
-      $kale_footer_copyright
+        $kale_footer_copyright
     ); ?></div>
     <?php } ?>
+
 <?php
 /*
     <div class="footer-copyright">
         <ul class="credit">
-            <li><?php _e('Built using ', 'kale'); ?><a href="https://www.lyrathemes.com/kale-pro"><?php _e('Kale Pro', 'kale'); ?></a> <?php _e('by', 'kale'); ?> <a href="https://www.lyrathemes.com">LyraThemes</a>.</li>
+            <li><?php _e(
+              'Built using ',
+              'kale'
+            ); ?><a href="https://www.lyrathemes.com/kale-pro"><?php _e(
+              'Kale Pro',
+              'kale'
+            ); ?></a> <?php _e(
+                'by',
+                'kale'
+              ); ?> <a href="https://www.lyrathemes.com">LyraThemes</a>.</li>
         </ul>
     </div>
+
 */
 ?>
 
